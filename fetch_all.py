ce5aac78f665b4c8422366ecf6509ce3e69eaf83
alifavif-cmd <alifavif@gmail.com>
2026-05-19 17:13:02 +0700

    Create fetch_all.py

---
+++ fetch_all.py
@@ -0,0 +1,7 @@
+{
+  "hk": { "result": "4509", "tgl": "19/05", "status": "sudah", "name": "Hongkong Pools" },
+  "hkl": { "result": "----", "tgl": "19/05", "status": "belum", "name": "Hongkong Lotto" },
+  "sgp": { "result": "2924", "tgl": "19/05", "status": "sudah", "name": "Singapore" },
+  "sdy": { "result": "4266", "tgl": "19/05", "status": "sudah", "name": "Sydney Pools" },
+  "sdl": { "result": "----", "tgl": "19/05", "status": "belum", "name": "Sydney Lotto" }
+}
